e40ea2214006e4dd42683609a0cd5ef23fda3109
bokal2 <bobbydasilva98@gmail.com>
2023-01-19 00:24:23 +0300

    Added order producer

---
+++ order_backend.py
@@ -0,0 +1,29 @@
+import json
+import time
+
+from kafka import KafkaProducer
+
+ORDER_KAFKA_TOPIC = "order_details"
+ORDER_LIMIT = 20
+
+PRODUCER = KafkaProducer(bootstrap_servers=["localhost:29092"],)
+
+class OrderProducer:
+    """ Order producer class """
+
+    print("Going to send order details to kafka topic: ", ORDER_KAFKA_TOPIC)
+
+    for i in range(1, ORDER_LIMIT):
+        order = {
+            "order_id": i,
+            "order_date": time.strftime("%Y-%m-%d %H:%M:%S"),
+            "order_amount": i * 100,
+            "order_status": "pending",
+        }
+        print("Sending order details: ", order)
+        PRODUCER.send(ORDER_KAFKA_TOPIC, json.dumps(order).encode("utf-8"))
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    OrderProducer()
